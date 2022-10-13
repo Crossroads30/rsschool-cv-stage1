f536d59eb682822e0781c51b674ab3b6bdf3dbf2
Crossroads30 <112560954+Crossroads30@users.noreply.github.com>
2022-10-13 11:41:17 +0300

    feat: add progress_line with activation

--- online-zoo/pages/main/main.js
+++ online-zoo/pages/main/main.js
@@ -1,1 +1,15 @@
-11
+
+function progress() {
+   let elem = document.getElementById('progress_line'),
+   width = 1,
+   id = setInterval(progressStatus, 60);
+   function progressStatus () {
+   	if (width  >= 100) {
+   		 clearInterval(id);
+   	} else {
+   		width ++;
+   		elem.style.width = width + '%';
+
+   	}
+   }
+}
