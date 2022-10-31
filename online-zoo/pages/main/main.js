b234f84520051d3e00b749633b9f8c5951ac0e62
Crossroads30 <112560954+Crossroads30@users.noreply.github.com>
2022-10-31 10:33:47 +0300

    feat: add adaptability to brakepoints

--- online-zoo/pages/main/main.js
+++ online-zoo/pages/main/main.js
@@ -13,6 +13,23 @@ function progress() {
    	}
    }
 };
+ 
+// burger_menu //
+
+/*const header_burger = document.querySelector('.header_burger');
+if(burger_menu){
+  const burger_menu = document.querySelector('.burger_menu');
+    burger_menu.addEventListener("click", function(e){
+      burger_menu.classList.toggle('_active');
+  }
+}
+
+/*$(document).ready(function() {
+  $('.header_burger').click(function(event) {
+    $('.header_burger','.burger_menu').toggleClass('active');
+  });
+});*/
+
 
 
 /*let validation = new JustValidate('#form')
